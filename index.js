4be93e78c5725f705de2db3d4bc5cebe13243ef7
3laaAl6ore <86629313+3laaAl6ore@users.noreply.github.com>
2022-03-07 10:19:06 +0200

    07.03.2022

---
+++ index.js
@@ -0,0 +1,29 @@
+//import express from 'express'; //another way
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const port = 6050;
+//const actionRoute = require("./controller/actions");
+
+// m52IqzYfCZhqwvs0      // password
+//mongodb+srv://shop_user:<password>@cluster0.qov5m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority //url
+
+//app.use("/api", actionRoute);
+
+const url =
+  "mongodb+srv://shop_user:m52IqzYfCZhqwvs0@cluster0.qov5m.mongodb.net/shop_db?retryWrites=true&w=majority";
+mongoose
+  .connect(url)
+  .then((res) => {
+    console.log(res);
+    app.listen(port, () => {
+      console.log(`conected on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
